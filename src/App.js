f2ee68a7eb81cde0fdc174c567a819663978f223
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2022-09-06 21:50:53 -0400

    gathering the goods from firestore

--- src/App.js
+++ src/App.js
@@ -4,27 +4,35 @@ import './App.css';
 import {db} from "./firebase-config"
 import {useState,useEffect} from "react"
 
-import{getDocs ,collection} from "firebase/firestore"
+import{getDocs ,collection, onSnapshot} from "firebase/firestore"
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const recipesCollectionRef = collection(db, "recipes")
 
-  // const [form, setForm] = useState({
-  //   title: "",
-  //   desc: "",
-  //   ingredients: [],
-  //   steps: []
-  // })
-  // const [popupActive, setPopupActive] = useState(false)
+  const [form, setForm] = useState({
+    title: "",
+    desc: "",
+    ingredients: [],
+    steps: []
+  })
+  const [popupActive, setPopupActive] = useState(false)
 
-  useEffect(() => {
-    const getRecipes = async () => {
-      const data = await getDocs(recipesCollectionRef);
-      console.log(data);
-    };
-    getRecipes();
-  },[]);
+
+
+useEffect (() => {
+  onSnapshot(recipesCollectionRef,snapshot => {
+    setRecipes(snapshot.docs.map(doc =>{
+      return{
+        id: doc.id,
+        viewing: false,
+        ...doc.data()
+      }
+    }))
+    
+    //console.log(snapshot)
+  })
+},[])
  
 
   return (
